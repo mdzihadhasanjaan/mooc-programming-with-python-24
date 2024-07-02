a55488807e90d720826d9a5154fb211ed9068681
Md Zihad Hasan Jaan <mdzihadhasanjaan@gmail.com>
2024-07-03 01:40:13 +0300

    no 13 and 14 added

--- part01-13_times_five/src/times_five.py
+++ part01-13_times_five/src/times_five.py
@@ -1,3 +1,4 @@
-number =int(input("Please type in a number:"))
-calculation= number * 5
-print(f"{number} times 5 is {calculation}")
+number = int(input())
+result = number * 5
+print(f"{number} times 5 is {result}")
+
